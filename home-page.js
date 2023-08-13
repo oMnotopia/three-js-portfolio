62fa5425e7808cafb498eabed524620d4bf338b3
Clayton <persinger.clayton@gmail.com>
2023-08-13 13:26:33 -0700

    Remove cube and plane, make line generator function

--- home-page.js
+++ home-page.js
@@ -21,33 +21,25 @@ camera.position.z = 50;
 const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.update();
 
-//Create plane
-const geomery = new THREE.PlaneGeometry(20, 20);
-const material = new THREE.MeshBasicMaterial({color: 0xff0000, side: THREE.DoubleSide});
-const plane = new THREE.Mesh(geomery, material);
-plane.rotation.set(-Math.PI / 2, 0, 0);
-
-
 //Create line
-const lineMat = new THREE.LineBasicMaterial({color: 0xffff00});
-const points = [];
-points.push(new THREE.Vector3(-30, -10, 0));
-points.push(new THREE.Vector3(-10, 0, 0));
-points.push(new THREE.Vector3(10, 10, 0));
-const lineGeo = new THREE.BufferGeometry().setFromPoints(points);
-const line = new THREE.Line(lineGeo, lineMat);
+const createLines = (startX, midX, finX) => {
+  const lineMat = new THREE.LineBasicMaterial({color: 0xffff00});
+  const points = [];
+  points.push(new THREE.Vector3(-30, -10, startX));
+  points.push(new THREE.Vector3(-20, 10, midX));
+  points.push(new THREE.Vector3(-10, 30, finX));
+  const lineGeo = new THREE.BufferGeometry().setFromPoints(points);
+  const line = new THREE.Line(lineGeo, lineMat);
+  scene.add(line);
+  return line;
+};
 
-
-//Attach line to plane
-scene.add(line);
-
-//Add objects to scene
-scene.add(plane);
+const line1 = createLines(-30, -20, -10);
+const line2 = createLines(40, 20, 0);
 
 const animate = () => {
-  //rotate the plane
-  plane.rotateZ(0.005);
-  line.rotateY(0.005);
+  line1.rotateY(0.005);
+  line2.rotateY(0.005);
   renderer.render(scene, camera);
 };
 renderer.setAnimationLoop(animate);
